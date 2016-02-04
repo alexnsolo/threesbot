fdde5d5cc444d6f2c3fd19ebd00cc0b45f340362
Alex Solo <alex.n.solo@gmail.com>
2016-02-03 21:25:22 -0800

    basic threes gameplay

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,8 +1,39 @@
+_ = lodash;
+
 angular.module('threes-bot', [
 	'angular-meteor'
 ]);
 
 angular.module('threes-bot').run(function() {
 	console.log('Threes Bot initialized.');
-	console.log(new Threes.Board());
+});
+
+angular.module('threes-bot').controller('BoardCtrl', function($scope) {
+	$scope.board = new Threes.Board();
+	console.log($scope.board);
+
+	$scope.canMove = function(direction) {
+		return $scope.board.isMoveValid(direction);
+	};
+
+	$scope.doMove = function(direction) {
+		if ($scope.canMove(direction)) {
+			$scope.board.moveInDirection(direction);
+		}
+	};
+
+	$scope.getTileClass = function(tile) {
+		if (tile === 0) {
+			return "";
+		}
+		else if (tile === 1) {
+			return "one";
+		}
+		else if (tile === 2) {
+			return "two";
+		}
+		else {
+			return "number";
+		}
+	};
 });
